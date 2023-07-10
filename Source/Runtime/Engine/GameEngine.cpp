2ca0a771241017d9093136a0723cd64fe68974dd
Vrixic21 <vrijpatel@icloud.com>
2023-07-10 13:36:27 -0400

    Updated GameEngine class and added more delegates for input bindings

--- Source/Runtime/Engine/GameEngine.cpp
+++ Source/Runtime/Engine/GameEngine.cpp
@@ -40,10 +40,46 @@ void VGameEngine::OnWindowEvent(WindowEvent& inWindowEvent)
 {
     WindowEventDispatcher EventDispatcher(inWindowEvent);
     EventDispatcher.Dispatch<WindowResizeEvent>(VE_BIND_EVENT_FUNC(OnWindowResized));
+    EventDispatcher.Dispatch<MouseMovedEvent>(VE_BIND_EVENT_FUNC(OnMouseMoved));
+    EventDispatcher.Dispatch<MouseButtonPressedEvent>(VE_BIND_EVENT_FUNC(OnMouseButtonPressed));
+    EventDispatcher.Dispatch<MouseButtonReleasedEvent>(VE_BIND_EVENT_FUNC(OnMouseButtonReleased));
+    EventDispatcher.Dispatch<MouseScrolledEvent>(VE_BIND_EVENT_FUNC(OnMouseScrolled));
+    EventDispatcher.Dispatch<KeyPressedEvent>(VE_BIND_EVENT_FUNC(OnKeyPressed));
+    EventDispatcher.Dispatch<KeyReleasedEvent>(VE_BIND_EVENT_FUNC(OnKeyReleased));
 }
 
 bool VGameEngine::OnWindowResized(WindowResizeEvent& inWindowResizeEvent)
 {
     FExtent2D Extent2D = { inWindowResizeEvent.GetWidth(), inWindowResizeEvent.GetHeight() };
     return Renderer::Get().OnRenderViewportResized(Extent2D);
+}
+
+bool VGameEngine::OnMouseButtonPressed(MouseButtonPressedEvent& inMouseEvent)
+{
+    return Renderer::Get().OnMouseButtonPressed(inMouseEvent);
+}
+
+bool VGameEngine::OnMouseButtonReleased(MouseButtonReleasedEvent& inMouseEvent)
+{
+    return Renderer::Get().OnMouseButtonReleased(inMouseEvent);
+}
+
+bool VGameEngine::OnMouseMoved(MouseMovedEvent& inMouseEvent)
+{
+    return Renderer::Get().OnMouseMoved(inMouseEvent);
+}
+
+bool VGameEngine::OnMouseScrolled(MouseScrolledEvent& inMouseEvent)
+{
+    return Renderer::Get().OnMouseScrolled(inMouseEvent);
+}
+
+bool VGameEngine::OnKeyPressed(KeyPressedEvent& inKeyPressedEvent)
+{
+    return Renderer::Get().OnKeyPressed(inKeyPressedEvent);
+}
+
+bool VGameEngine::OnKeyReleased(KeyReleasedEvent& inKeyReleasedEvent)
+{
+    return Renderer::Get().OnKeyReleased(inKeyReleasedEvent);
 }
