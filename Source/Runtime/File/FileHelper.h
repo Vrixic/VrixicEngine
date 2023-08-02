da20b7deddb0e6b93ff79b5d596f108eb928b222
Vrixic21 <vrijpatel@icloud.com>
2023-08-02 15:39:34 -0400

    Added more helper functions for file reading, checking, and writing
    UPDATED:
    - FileHelper.h: Added more helper functions (For folder creation/deletion)
    - FileReader.h: Now stores the size of the file as well

--- Source/Runtime/File/FileHelper.h
+++ Source/Runtime/File/FileHelper.h
@@ -1,5 +1,5 @@
 /**
-* This file is part of the "Vrixic Engine" project (Copyright (c) 2022-2023 by Vrij Patel) 
+* This file is part of the "Vrixic Engine" project (Copyright (c) 2022-2023 by Vrij Patel)
 * See "LICENSE.txt" for license information.
 */
 
@@ -9,34 +9,162 @@
 #include <string>
 
 /**
-* Static class which has helper functions for reading files 
+* Static class which has helper functions for reading files
 */
 class VRIXIC_API FileHelper
 {
 public:
-	// Loads a text file into a string 
-	static bool LoadFileToString(std::string& outResult, std::string& inFilePath)
-	{
+    /**
+    * Loads a text file into a string
+    */
+    static bool LoadFileToString(std::string& outResult, std::string& inFilePath)
+    {
         FileReader Reader(inFilePath); //= FileManager::GetInstance().CreateFileReader(inFilePath);
         if (!Reader.IsOpen())
         {
             return false;
         }
-        
-        Reader.Seek(0, EFileSeek::End);
 
-		uint64 Size = Reader.Tell();
-		if (Size == 0)
-		{
-			return false;
-		}
-
-		Reader.Seek(0, EFileSeek::Begin);
+        uint64 Size = Reader.Size();
+        if (Size == 0)
+        {
+            return false;
+        }
 
         outResult.resize(Size + 1);
-		Reader.Read(&outResult[0], Size);
+        Reader.Read(&outResult[0], Size);
 
-		Reader.Close();
-		return true;
-	}
+        Reader.Close();
+        return true;
+    }
+
+    /**
+    * Loads a file into a contiguos array of bytes
+    * @note: assumes that memory is already allocated
+    */
+    static bool LoadFileToBytes(char* outResult, std::string& inFilePath)
+    {
+        FileReader Reader(inFilePath); //= FileManager::GetInstance().CreateFileReader(inFilePath);
+        if (!Reader.IsOpen())
+        {
+            return false;
+        }
+
+        uint64 Size = Reader.Size();
+        if (Size == 0)
+        {
+            return false;
+        }
+
+        Reader.Read(&outResult[0], Size);
+        Reader.Close();
+        return true;
+    }
+
+    /**
+    * Overwrites bytes to the file specified
+    */
+    static bool WriteBytesToFile(char* inData, uint64 inDataSize, std::string& inFileToWrite)
+    {
+        FILE* File = fopen(inFileToWrite.c_str(), "wb");
+
+        if (File == nullptr) return false;
+
+        fwrite(inData, inDataSize, 1, File);
+        fclose(File);
+
+        return true;
+    }
+
+    /**
+    * Creates a folder[directory], only Window Supported
+    */
+    static bool CreateFolder(const std::string& inFilePath)
+    {
+#if defined(_WIN64)
+        int Result = CreateDirectoryA(inFilePath.c_str(), NULL);
+        return Result != 0;
+#else
+        VE_STATIC_ASSERT(false, VE_TEXT("[FileHelper]: Only windows folder supported..."));
+#endif // _WIN64
+    }
+
+    /**
+    * Returns as an out param the current directory path 
+    */
+    static void GetCurrentFolder(char* outDirectoryPath)
+    {
+#if defined(_WIN64)
+        DWORD written_chars = GetCurrentDirectoryA(512, outDirectoryPath);
+        outDirectoryPath[written_chars] = 0;
+#else
+        VE_STATIC_ASSERT(false, VE_TEXT("[FileHelper]: Only windows folder supported..."));
+#endif // _WIN64
+    }
+
+    /**
+    * Sets the current directory to the one specified if it is valid 
+    */
+    static void SetCurrentFolder(const std::string inFilePath)
+    {
+#if defined(_WIN64)
+        if (!SetCurrentDirectoryA(inFilePath.c_str())) {
+            VE_ASSERT(false, VE_TEXT("[FileHelper]: Cannot change current folder[directory] to {0}"), inFilePath.c_str());
+        }
+#else
+        VE_STATIC_ASSERT(false, VE_TEXT("[FileHelper]: Only windows folder supported..."));
+#endif // _WIN64
+    }
+
+    /**
+    * Checks if a folder exists
+    */
+    static bool DoesFolderExist(const std::string& inFilePath)
+    {
+#if defined(_WIN64)
+        WIN32_FILE_ATTRIBUTE_DATA Data;
+        return GetFileAttributesExA(inFilePath.c_str(), GetFileExInfoStandard, &Data);
+#else
+        VE_STATIC_ASSERT(false, VE_TEXT("[FileHelper]: Only windows folder supported..."));
+#endif // _WIN64
+    }
+
+    /**
+    * Deletes a folder[directory], only Window Supported
+    */
+    static bool DeleteFolder(const std::string& inFilePath)
+    {
+#if defined(_WIN64)
+        int Result = RemoveDirectoryA(inFilePath.c_str());
+        return Result != 0;
+#else
+        VE_STATIC_ASSERT(false, VE_TEXT("[FileHelper]: Only windows folder supported..."));
+#endif // _WIN64
+    }
+
+    /**
+    * Checks if a file exists
+    */
+    static bool DoesFileExist(const std::string& inFilePath)
+    {
+#if defined(_WIN64) || defined(_WIN32)
+        WIN32_FILE_ATTRIBUTE_DATA Data;
+        return GetFileAttributesExA(inFilePath.c_str(), GetFileExInfoStandard, &Data);
+#else
+        FileReader Reader(inFilePath);
+        bool IsOpen = Reader.IsOpen();
+        Reader.Close();
+
+        return IsOpen;
+#endif // _WIN64
+    }
+
+    /**
+    * Deletes a file, only Window Supported
+    */
+    static bool DeleteFileFromPath(const std::string& inFilePath)
+    {
+        int Result = remove(inFilePath.c_str());
+        return Result != 0;
+    }
 };
--- Source/Runtime/File/FileReader.h
+++ Source/Runtime/File/FileReader.h
@@ -1,5 +1,5 @@
 /**
-* This file is part of the "Vrixic Engine" project (Copyright (c) 2022-2023 by Vrij Patel) 
+* This file is part of the "Vrixic Engine" project (Copyright (c) 2022-2023 by Vrij Patel)
 * See "LICENSE.txt" for license information.
 */
 
@@ -12,23 +12,23 @@
 
 enum class EFileOpenFlags
 {
-	Read, // Read the file
-	Write, // Write to file, overwrites its previous contents 
-	Append // Add to the pre-existing contents of the file
+    Read, // Read the file
+    Write, // Write to file, overwrites its previous contents 
+    Append // Add to the pre-existing contents of the file
 };
 
 enum class EFileSeek
 {
-	Begin,
-	Current,
-	End
+    Begin,
+    Current,
+    End
 };
 
 enum class EFileReadResult
 {
-	Success,
-	Error,
-	OutOfBytesToRead
+    Success,
+    Error,
+    OutOfBytesToRead
 };
 
 /**
@@ -37,103 +37,136 @@ enum class EFileReadResult
 class VRIXIC_API FileReader
 {
 public:
-	FileReader(std::string inPath)
-	{
+    FileReader(std::string inPath)
+    {
         Handle = { };
-		Handle.open(inPath);
-	}
+        Handle.open(inPath);
 
-	~FileReader()
-	{
-		Handle.close();
-	}
+        // Get the size of the file 
+        SeekEnd(0);
+        SizeInBytes = Tell();
+        SeekBegin(0);
+    }
 
-public:
-	/**
-	* To to a section into the file 
-	* 
-	* @param inOffset - offset reative to EFileSeek param 'inFileSeek'
-	* @param inFileSeek - position where it should seek to 
-	*/
-	void Seek(uint32 inOffset, EFileSeek inFileSeek)
-	{
-		switch (inFileSeek)
-		{
-		case EFileSeek::Begin:
-			Handle.seekg(inOffset, std::ios::beg);
-			break;
-		case EFileSeek::End:
-			Handle.seekg(inOffset, std::ios::end);
-			break;
-		}		
-	}
-
-	/**
-	* Reads bytes from file
-	*
-	* @param outResult - bytes read will be stored in this variable
-	* @param inSize - count of bytes to read 
-	*/
-	void Read(char* outResult, uint64 inSize)
-	{
-		Handle.read(outResult, inSize);
-	}
-
-	/**
-	* Read a byte from file
-	*
-	* @param outResult - byte read will be stored in this variable
-	*/
-	EFileReadResult Read8(uint8* outResult)
-	{
-		if (Handle.eof())
-		{
-			return EFileReadResult::OutOfBytesToRead;
-		}
-
-		Handle.read((char*)outResult, sizeof(uint8));
-
-		return EFileReadResult::Success;
-	}
-
-	/**
-	* Reads four bytes from file
-	*
-	* @param outResult - bytes read will be stored in this variable
-	*/
-	EFileReadResult Read32(uint32* outResult)
-	{
-		if (Handle.eof())
-		{
-			return EFileReadResult::OutOfBytesToRead;
-		}
-
-		Handle.read((char*)outResult, sizeof(uint32));
-
-		return EFileReadResult::Success;
-	}
-
-	/**
-	* @return uint64 - return the position of the current character
-	*/
-	uint64 Tell()
-	{
-		return Handle.tellg();
-	}
-
-	void Close()
-	{
-		Handle.close();
-	}
+    ~FileReader()
+    {
+        Handle.close();
+    }
 
 public:
+    /**
+    * Seek to a section into the file
+    *
+    * @param inOffset - offset reative to EFileSeek param 'inFileSeek'
+    * @param inFileSeek - position where it should seek to
+    */
+    void Seek(uint64 inOffset, EFileSeek inFileSeek)
+    {
+        switch (inFileSeek)
+        {
+        case EFileSeek::Begin:
+            Handle.seekg(inOffset, std::ios::beg);
+            break;
+        case EFileSeek::End:
+            Handle.seekg(inOffset, std::ios::end);
+            break;
+        }
+    }
 
-	bool IsOpen()
-	{
-		return Handle.is_open();
-	}
+    /**
+    * Seek to a the beginning of a file
+    *
+    * @param inOffset - offset reative to the beginning of the file 
+    */
+    inline void SeekBegin(uint64 inOffset)
+    {
+        Handle.seekg(inOffset, std::ios::beg);
+    }
+
+    /**
+    * Seek to a the end of a file
+    *
+    * @param inOffset - offset reative to end of the file 
+    */
+    inline void SeekEnd(uint64 inOffset)
+    {
+        Handle.seekg(inOffset, std::ios::end);
+    }
+
+    /**
+    * Reads bytes from file
+    *
+    * @param outResult - bytes read will be stored in this variable
+    * @param inSize - count of bytes to read
+    */
+    void Read(char* outResult, uint64 inSize)
+    {
+        Handle.read(outResult, inSize);
+    }
+
+    /**
+    * Read a byte from file
+    *
+    * @param outResult - byte read will be stored in this variable
+    */
+    EFileReadResult Read8(uint8* outResult)
+    {
+        if (Handle.eof())
+        {
+            return EFileReadResult::OutOfBytesToRead;
+        }
+
+        Handle.read((char*)outResult, sizeof(uint8));
+
+        return EFileReadResult::Success;
+    }
+
+    /**
+    * Reads four bytes from file
+    *
+    * @param outResult - bytes read will be stored in this variable
+    */
+    EFileReadResult Read32(uint32* outResult)
+    {
+        if (Handle.eof())
+        {
+            return EFileReadResult::OutOfBytesToRead;
+        }
+
+        Handle.read((char*)outResult, sizeof(uint32));
+
+        return EFileReadResult::Success;
+    }
+
+    inline void Close()
+    {
+        Handle.close();
+    }
+
+public:
+
+    /**
+    * @return uint64 - return the position of the current character
+    */
+    inline uint64 Tell()
+    {
+        return Handle.tellg();
+    }
+
+    inline uint64 Size() const
+    {
+        return SizeInBytes;
+    }
+
+    inline bool IsOpen() const 
+    {
+        return Handle.is_open();
+    }
 
 private:
     std::string Path;
     std::ifstream Handle;
+
+    /** Size of the file in bytes */
+    uint64 SizeInBytes;
 };
